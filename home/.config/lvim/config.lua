c05ebcbae0a8eb9fb70679d575bdb74d4ef6bbb1
Ben Yap <contact@benyap.com>
2021-10-17 18:17:09 +1100

    feat(lvim): tweaks

--- home/.config/lvim/config.lua
+++ home/.config/lvim/config.lua
@@ -12,24 +12,14 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
-
--- general custom
 vim.opt.relativenumber = true
-vim.opt.hlsearch = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
+
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
--- override default mappings
-lvim.builtin.which_key.mappings.c = {
-  -- remove the bang so that it doesn't close without saving
-  "<cmd>write<cr><cmd>BufferClose<cr>", "Save & Close"
-}
-lvim.builtin.which_key.mappings.x =  {
-  "<cmd>BufferClose<cr>", "Close Buffer"
-}
+lvim.keys.normal_mode["<C-r>"] = ":redo<cr>"
 
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
@@ -37,29 +27,50 @@ lvim.builtin.which_key.mappings.x =  {
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+lvim.builtin.telescope.on_config_done = function()
+  local actions = require "telescope.actions"
+  -- for input mode
+  lvim.builtin.telescope.mappings.i["<C-p>"] = actions.cycle_history_prev
+  lvim.builtin.telescope.mappings.i["<C-j>"] = actions.move_selection_next
+  lvim.builtin.telescope.mappings.i["<C-k>"] = actions.move_selection_previous
+  lvim.builtin.telescope.mappings.i["<C-n>"] = actions.cycle_history_next
+  -- for normal mode
+  lvim.builtin.telescope.mappings.n["<C-j>"] = actions.move_selection_next
+  lvim.builtin.telescope.mappings.n["<C-k>"] = actions.move_selection_previous
+  -- ignore directories when searching
+  lvim.builtin.telescope.defaults.vimgrep_arguments = {
+    "rg",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    "--hidden",
+    "--no-ignore",
+    "--iglob='node_modules/*'",
+    "--iglob='.git/*'",
+    "--iglob='.next/*'"
+  }
+end
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
+lvim.builtin.which_key.mappings.c = { "<cmd>write<cr><cmd>BufferClose<cr>", "Save & Close" }
+lvim.builtin.which_key.mappings.x = { "<cmd>BufferClose<cr>", "Close Buffer" }
+
+lvim.builtin.which_key.mappings.f = { "<cmd>Telescope find_files find_command=fd,--type=file,--hidden,--no-ignore,--exclude=node_modules/,--exclude=.git/,--exclude=.next/<cr>", "Find File" }
+lvim.builtin.which_key.mappings.F = { "<cmd>Telescope live_grep<cr>", "Find Text" }
+lvim.builtin.which_key.mappings.P = lvim.builtin.which_key.mappings.p
+lvim.builtin.which_key.mappings.p = { "<cmd>Telescope projects<cr>", "Projects" }
+
+lvim.builtin.which_key.mappings.t = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -67,6 +78,8 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
+lvim.builtin.nvimtree.ignore = { ".git", "*.pyc", ".DS_Store" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -126,13 +139,12 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+    {
+      "folke/trouble.nvim",
+      cmd = "TroubleToggle",
+    },
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
