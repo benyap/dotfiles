aa15afcfd82b2ee6f63c5e614d1753e370f2dc6e
Ben Yap <contact@benyap.com>
2021-10-18 23:16:00 +1100

    fix(tasks): iterate through symlinks properly

--- scripts/tasks/setup_symlinks.sh
+++ scripts/tasks/setup_symlinks.sh
@@ -26,63 +26,63 @@ setup_symlinks() {
     ".zshrc.local"
   )
 
-  _create_directories "${directories[@]}"
-  _create_symlinks "${links[@]}"
+  # Create directories for symlinks
+  for dir in "${directories[@]}"; do
+    _create_directory "$dir"
+  done
+
+  # Create symlinks
+  for link in "${links[@]}"; do
+    _create_symlink "$link"
+  done
 }
 
-_create_directories() {
-  local -r dirs="$1"
+_create_directory() {
+  local -r directory="$1"
 
   local path=""
   local pathEscaped=""
 
-  # Create directories for symlinks
-  for dir in "${dirs[@]}"; do
-    path="$HOME/$dir"
-    pathEscaped="$(escape_path_containing_spaces "$path")"
+  path="$HOME/$directory"
+  pathEscaped="$(escape_path_containing_spaces "$path")"
 
-    if ! test -d "$path"; then
-      execute "mkdir -p $pathEscaped" "Create directory $path"
-    else
-      print_ok "Directory exists $path"
-    fi
-  done
+  if ! test -d "$path"; then
+    execute "mkdir -p $pathEscaped" "Create directory $path"
+  else
+    print_ok "Directory exists $path"
+  fi
 }
 
-_create_symlinks() {
-  local -r symlinks="$1"
- 
-  local link=""
+_create_symlink() {
+  local -r link="$1"
+
   local sourceFile=""
   local targetFile=""
   local sourceFileEscaped=""
   local targetFileEscaped=""
 
-  # Create symlinks
-  for link in "${symlinks[@]}"; do
-    sourceFile="$(cd .. && pwd)/home/$link"
-    targetFile="$HOME/$link"
+  sourceFile="$(cd .. && pwd)/home/$link"
+  targetFile="$HOME/$link"
 
-    sourceFileEscaped="$(escape_path_containing_spaces "$sourceFile")"
-    targetFileEscaped="$(escape_path_containing_spaces "$targetFile")"
+  sourceFileEscaped="$(escape_path_containing_spaces "$sourceFile")"
+  targetFileEscaped="$(escape_path_containing_spaces "$targetFile")"
 
-    # If the symlink doesn't exist, create it
-    if ! test -e "$targetFile"; then
+  # If the symlink doesn't exist, create it
+  if ! test -e "$targetFile"; then
+    execute "ln -fs $sourceFileEscaped $targetFileEscaped" "Link $targetFile"
+
+  # If the symlink is already and is correct, then do nothing
+  elif test "$(readlink "$targetFile")" == "$sourceFile"; then
+    print_ok "Already linked $targetFile"
+
+  # Otherwise, check if we want to overwrite the existing file
+  else
+    ask_for_confirmation "$targetFile already exists, do you want to overwrite it?"
+    if answer_is_yes; then
+      rm -rf "$targetFile"
       execute "ln -fs $sourceFileEscaped $targetFileEscaped" "Link $targetFile"
-
-    # If the symlink is already and is correct, then do nothing
-    elif test "$(readlink "$targetFile")" == "$sourceFile"; then
-      print_ok "Already linked $targetFile"
-
-    # Otherwise, check if we want to overwrite the existing file
     else
-      ask_for_confirmation "$targetFile already exists, do you want to overwrite it?"
-      if answer_is_yes; then
-        rm -rf "$targetFile"
-        execute "ln -fs $sourceFileEscaped $targetFileEscaped" "Link $targetFile"
-      else
-        print_error "Link $targetFile"
-      fi
+      print_error "Link $targetFile"
     fi
-  done
+  fi
 }
