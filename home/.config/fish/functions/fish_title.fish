9a049c52f2632d90c1785fd3189f5ffb4ff85cab
Ben Yap <contact@benyap.com>
2022-05-13 23:06:38 +1000

    chore: tweak fish_title

--- home/.config/fish/functions/fish_title.fish
+++ home/.config/fish/functions/fish_title.fish
@@ -2,11 +2,11 @@ function fish_title
     set --local repository_url (git remote get-url --all origin 2>/dev/null)
     if test -z "$repository_url"
         if [ "$PWD" = "$HOME" ]
-            echo [home]
+            echo "~"
         else
-            echo [dir] (pwd | string match -r '[^/]*$')
+            echo (pwd | string match -r '[^/]*$')
         end
     else
-        echo [git] (echo $repository_url | string match -r '[^/]*$' | string replace -r '\.git$' '')
+        echo git @ (echo $repository_url | string match -r '[^/]*$' | string replace -r '\.git$' '')
     end
 end
