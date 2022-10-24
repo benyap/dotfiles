37fc8ef730a3ea2246fdfc259b3148f1101328e1
Ben Yap <contact@benyap.com>
2022-10-25 10:53:39 +1100

    feat: add 1Password CLI

--- scripts/tasks/install_apps_brew/1password.sh
+++ scripts/tasks/install_apps_brew/1password.sh
@@ -2,5 +2,5 @@
 
 install_1password() {
   brew_install_app "1Password 8" "1password"
-  # brew_install "1Password CLI" "1password-cli"
+  brew_install_app "1Password CLI" "1password/tap/1password-cli"
 }
