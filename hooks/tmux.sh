bd355d5933bb1b24b315246a81546db67ddf7491
Ben Yap <19235373+benyap@users.noreply.github.com>
2026-08-24 09:26:40 +1000

    chore: fix bad tpm install directory

--- hooks/tmux.sh
+++ hooks/tmux.sh
@@ -1,9 +1,9 @@
 post_stow() {
-    if [[ ! -d "~/.config/tmux/plugins/tpm" ]]; then
+    if [[ ! -d "$HOME/.config/tmux/plugins/tpm" ]]; then
       echo "⏳ Cloning: tmux-plugins/tpm..."
-      git clone "https://github.com/tmux-plugins/tpm" "~/.config/tmux/plugins/tpm"
+      git clone "https://github.com/tmux-plugins/tpm" "$HOME/.config/tmux/plugins/tpm"
       echo "✅ Cloned: tmux-plugins/tpm"
     else
-      echo "🆗 Directory ~/.config/tmux/plugins/tpm already exists"
+      echo "🆗 Directory $HOME/.config/tmux/plugins/tpm already exists"
     fi
 }
